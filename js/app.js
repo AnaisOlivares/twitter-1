b653d194ec561fdd33a7e4acae1c1186959edc9e
AnaisOlivares <anaisolivareshilario@gmail.com>
2017-12-20 09:44:23 -0500

    modificando, no yet

--- js/app.js
+++ js/app.js
@@ -1,3 +1,75 @@
+$(document).ready(function() {
+  var msg = $('#msg');
+  var msgContainer = $('#msg-container');
+  var btn = $('#btn');
+  var counter = $('#counter');
+  var limit = 140;
+  var txt = '';
+
+  $('#btn').click(function(){
+    if (msg.val() !== ''){
+     var  message = $('<p>');
+      message.addClass('paragraph');
+      message.html() = msg.val() + '<i> Posted ' + moment().format('hh:mm') + '</i>';
+      msgContainer.appendTo(message);
+      msg.val() = '';
+    }
+  });
+// añadiendo otro evento
+msg.keydown(function(){
+  var letters = msg.val().length;
+  if(letters <= 0 || letters > limit){
+    btn.disabled = true;
+    btn.$('.class').remove('btn-active');
+    btn.$('.class').addClass('btn-inactive');
+  }else if(letters < limit || letters > 0) {
+    btn.disabled = false;
+    btn.$('.class').prop('btn-active');
+    btn.$('.class').prop('btn-inactive');
+    txt = msg.val();
+});
+  // msg.addEven.$('.class').Listener('keydown',validateLength);
+  // msg.addEven.$('.class').Listener('keyup', validateLength);
+  //
+  // function validateLength(){
+  //   var letters = msg.value.length;
+  //   if(letters <= 0 || letters > limit){
+  //     btn.disabled = true;
+  //     btn.classList.remove('btn-active');
+  //     btn.classList.add('btn-inactive');
+  //   }else if(letters < limit || letters > 0) {
+  //     btn.disabled = false;
+  //     btn.classList.add('btn-active');
+  //     btn.classList.remove('btn-inactive');
+  //     txt = msg.value;
+  //   }
+
+    if(letters > 120 && letters < 130){
+      counter.setAttribute('style', 'color: purple');
+    } else if(letters > 130 && letters <= 140){
+      counter.setAttribute('style', 'color: #117864');
+    }else if(letters <= 120){
+      counter.setAttribute('style', 'color: black');
+    }
+
+    counter.value = limit - letters;
+
+    msg.addEventListener('keydown', autosize);
+    function autosize(){
+    var resize = this;
+    setTimeout(function(){
+    resize.style.cssText = 'height:auto; padding:0';
+    resize.style.cssText = 'height:' + resize.scrollHeight + 'px';
+    },0);
+    }
+  };
+});
+
+
+
+
+
+
 window.onload =addEventListener('load', function(event){
   var msg = document.getElementById('msg');
   var btn = document.getElementById('btn');
@@ -5,7 +77,7 @@ window.onload =addEventListener('load', function(event){
   var counter = document.getElementById('counter');
   var limit = 140;
   var txt = '';
-  
+
   btn.addEventListener('click', function(){
     if(msg.value !== ''){
       var message = document.createElement('p');
@@ -39,7 +111,7 @@ window.onload =addEventListener('load', function(event){
     }else if(letters <= 120){
       counter.setAttribute('style', 'color: black');
     }
-    
+
     counter.value = limit - letters;
 
     msg.addEventListener('keydown', autosize);
